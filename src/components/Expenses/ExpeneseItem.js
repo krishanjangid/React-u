8c3f5d80022dfd80409d0149c88f1ec650261ce7
krishanjangid <krishanjangid516@gmail.com>
2023-02-17 23:53:31 +0530

    new update

--- src/components/Expenses/ExpeneseItem.js
+++ src/components/Expenses/ExpeneseItem.js
@@ -11,7 +11,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
         </div>
-        <button onClick={() => {console.log('Clicked')}}>Change Title</button>
+        <button onClick={() => {console.log('Clicked!!!')}}>Change Title</button>
     </Card>
     );
 }
