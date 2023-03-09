71b83dab00e3f98b64dee1a7409c6036ff7b3459
krishanjangid <krishanjangid516@gmail.com>
2023-03-09 23:26:56 +0530

    button name changed from add expense to add product

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -68,7 +68,7 @@ const ExpenseForm = (props) => {
             </div>
         </div>
         <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
+            <button type="submit">Add Product</button>
         </div>
     </form>
 };
